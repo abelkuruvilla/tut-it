d78207139364b81815be5169e417f00e78bf2b98
Abel Kuruvilla <abelkoottothara@gmail.com>
2016-09-06 23:12:22 +0530

    6

---
+++ Assets/Scripts/TestScore.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TestScore : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+
+
+	// Update is called once per frame
+	void Update () {
+		StartCoroutine (updateScore ());
+	}
+
+	IEnumerator updateScore(){
+		yield return new WaitForSeconds (1f);
+		GetComponent<Text> ().text =((int) Time.time).ToString ();
+	}
+}
