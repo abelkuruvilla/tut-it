01ecdec363e5fa69501c986bfa3d5aa2cd1670aa
Abel Kuruvilla <abelkoottothara@gmail.com>
2016-09-08 00:11:48 +0530

    fr

---
+++ Assets/Scripts/ObstacleG.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObstacleG : MonoBehaviour {
+
+
+	public GameObject[] obstacles;
+
+
+
+	// Use this for initialization
+	void Start () {
+		obstacles =(GameObject[]) Resources.LoadAll ("");
+		for (int i = 0; i < 5; i++) {
+			Instantiate (obstacles [i]);
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
